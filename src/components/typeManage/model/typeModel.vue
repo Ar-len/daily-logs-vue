9dd3b51ee9a65fd405c18be87d27964d9765dcc1
禹星星 <yuxingxing@ktwlrj.com>
2020-05-08 12:27:34 +0800

    增加组件

---
+++ src/components/typeManage/model/typeModel.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-modal title="异步关闭的 Modal 对话框" :visible="visible" @ok="handleAsyncOk" @cancel="handleAsyncCancel"
+    :confirm-loading="asyncConfirmLoading">
+    <p>对话框的内容</p>
+    <p>对话框的内容</p>
+    <p>对话框的内容</p>
+  </v-modal>
+</template>
+
+<script>
+export default {
+  props: ['visible'],
+  data () {
+    return {
+      asyncConfirmLoading: false
+    }
+  },
+  created () {
+    console.log(this.typeModalVisible)
+  },
+  methods: {
+    showAsyncModal () {
+      this.typeModalVisible = true
+    },
+
+    handleAsyncOk () {
+      /* 对话框将在两秒后关闭 */
+      this.asyncConfirmLoading = true
+      setTimeout(() => {
+        this.typeModalVisible = false
+        this.asyncConfirmLoading = false
+      }, 2000)
+    },
+
+    handleAsyncCancel () {
+      this.typeModalVisible = false
+    }
+  }
+}
+
+</script>
--- src/components/typeManage/type.vue
+++ src/components/typeManage/type.vue
@@ -1,45 +1,61 @@
 <template>
-    <div>
-        <v-more-panel>
-        <v-form slot="form">
+  <div>
+    <v-more-panel>
+      <v-form slot="form">
         <v-form-item label="用户名">
-            <v-input placeholder="请输入用户名"></v-input>
+          <v-input placeholder="请输入用户名"></v-input>
         </v-form-item>
         <v-form-item label="密码">
-            <v-input type="password" placeholder="请输入密码"></v-input>
+          <v-input type="password" placeholder="请输入密码"></v-input>
         </v-form-item>
         <v-form-item label="机构编码">
-            <v-input placeholder="请输入机构编码"></v-input>
+          <v-input placeholder="请输入机构编码"></v-input>
         </v-form-item>
         <v-form-item label="年龄">
-            <v-input placeholder="请输入年龄"></v-input>
+          <v-input placeholder="请输入年龄"></v-input>
         </v-form-item>
         <v-form-item label="手机">
-            <v-input placeholder="请输入手机号码"></v-input>
+          <v-input placeholder="请输入手机号码"></v-input>
         </v-form-item>
-        </v-form>
-        <v-button slot="control" type="primary" html-type="button" icon="search">查询</v-button>
+      </v-form>
+      <v-button slot="control" type="primary" html-type="button" icon="search">查询</v-button>
     </v-more-panel>
-    <v-data-table :data='loadData' :columns='longColumns' :fixed-left="1" :fixed-right="1">
-        <template slot="td" slot-scope="props">
-        <div v-if="props.column.field=='action'">
-            <v-icon type="play-circle"></v-icon>
-            <v-icon type="download"></v-icon>
-            <v-icon type="link"></v-icon>
-            <v-icon type="heart-o"></v-icon>
-            <v-icon type="share-alt"></v-icon>
+    <v-data-table :data='loadData' :columns='longColumns' :fixed-left="0" :fixed-right="1">
+      <template slot="td" slot-scope="props">
+        <div v-if="props.column.field=='action'" class="text-center">
+          <v-button-group>
+            <v-button type="info" title="查看" @click="viewType(props.item)">
+              <v-icon type="file-text"></v-icon>
+            </v-button>
+            <v-button type="primary" title="编辑" @click="editType(props.item)">
+              <i class="el-icon-edit"></i>
+            </v-button>
+            <v-button type="info" title="下载" @click="downloadType(props.item)">
+              <v-icon type="download"></v-icon>
+            </v-button>
+            <v-button type="danger" title="删除" @click="delType(props.item)">
+              <i class="el-icon-delete"></i>
+            </v-button>
+          </v-button-group>
         </div>
         <span v-else v-html="props.content"></span>
-        </template>
+      </template>
+      <TypeModel :visible="typeModalVisible" />
     </v-data-table>
-    </div>
+  </div>
+
 </template>
 
 <script>
 import axios from 'axios'
+import TypeModel from '@/components/typeManage/model/typeModel.vue'
 export default {
+  components: {
+    TypeModel
+  },
   data: function () {
     return {
+      typeModalVisible: true,
       loadData (pramas) {
         return axios.get('static/datatable.json', pramas).then(res => {
           return res.data
@@ -69,8 +85,19 @@ export default {
     }
   },
   methods: {
-
+    viewType (pramas) {
+      this.typeModalVisible = true
+      console.log(pramas)
+      // this.typeModalVisible = !this.typeModalVisible
+      console.log(this.typeModalVisible)
+    }
   }
 }
 
 </script>
+<style scoped>
+  .text-center {
+    text-align: center;
+  }
+
+</style>
